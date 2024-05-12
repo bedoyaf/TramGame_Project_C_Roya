137d0c756629f9db266bd73eb368a359a23b6d63
rolloj <jakubrollo10@gmail.com>
2024-05-13 00:17:53 +0200

    Add sprites and walking and spawners

--- Tram_Game/Assets/Scripts/PlayerMovement_Inside.cs
+++ Tram_Game/Assets/Scripts/PlayerMovement_Inside.cs
@@ -5,10 +5,13 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] float speed = 0.5f;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,5 +30,7 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1f, 1f, 1f); // Facing left
         }
+
+        animator.SetBool("IsWalking", horizontalInput != 0);
     }
 }
