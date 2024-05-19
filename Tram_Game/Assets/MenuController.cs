b27ba1c284846a63dfd6e48a8b4f5e89829a2e43
@bedoyaf <filipbedoya64@gmail.com>
2024-05-19 21:42:54 +0200

    added tutorial

--- Tram_Game/Assets/MenuController.cs
+++ Tram_Game/Assets/MenuController.cs
@@ -7,6 +7,12 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     public string startScene;
+
+    bool showing_inside = true;
+    public GameObject Menu;
+    public GameObject Tutorial;
+    public GameObject TutorialOutside;
+    public GameObject TutorialInside;
     void Start()
     {
 
@@ -25,5 +31,36 @@ public class MenuController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void show_tutorial()
+    {
+        showing_inside=true;
+        TutorialOutside.SetActive(false);
+        TutorialInside.SetActive(true);
+        Menu.SetActive(false);
+        Tutorial.SetActive(true);
+    }
+    public void hide_tutorial() {
+        showing_inside = true;
+        TutorialOutside.SetActive(false);
+        TutorialInside.SetActive(false);
+        Menu.SetActive(true);
+        Tutorial.SetActive(false);
+    }
+    public void switch_to_inside_or_outside()
+    {
+        if(showing_inside)
+        {
+            showing_inside=false;
+            TutorialOutside.SetActive(true);
+            TutorialInside.SetActive(false);
+        }
+        else
+        {
+            showing_inside = true;
+            TutorialOutside.SetActive(false);
+            TutorialInside.SetActive(true);
+        }
     }
 }
